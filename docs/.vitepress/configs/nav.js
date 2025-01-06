af8184e30d15377455dd6c9aa79c26c3f27400e3
angular <1921934563@qq.com>
2025-01-06 13:59:06 +0800

    feat: nav

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -1,5 +1,3 @@
 export default [
-  // { text: '文档', link: '/guide/', activeMatch: '^/guide/' },
-  // { text: '组件', link: '/components/button', activeMatch: '^/components/' },
-  // { text: 'API 文档', link: '/' }
+  { text: 'Github', link: 'https://github.com/CZL-AI'}
 ]
